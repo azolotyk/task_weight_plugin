3aef9b632052b75f58530a8659dd99aa57a7068f
Золотых Анастасия Андреевна <a.zolotykh@2gis.ru>
2025-01-29 17:12:45 +0300

    ability to combine and copy text is added

--- popup.js
+++ popup.js
@@ -1,9 +1,11 @@
+// Формула расчёта оценки для задач техдолга
 function techdebtMeasurement() {
     let tabs_indexes = ['1', '2', '3', '4', '5', '6'];
     tabs_indexes.forEach((id) => {
-        let innovation = +document.querySelector('input[name="innovation"][panel_index="'+id+'"]:checked').value;
-        let risk = +document.querySelector('input[name="risk"][panel_index="'+id+'"]:checked').value;
-        let level = +document.querySelector('input[name="level"][panel_index="'+id+'"]:checked').value;
+        let panel = document.getElementById('panel-' + id);
+        let innovation = +panel.querySelector('input[name="innovation"]:checked').value;
+        let risk = +panel.querySelector('input[name="risk"]:checked').value;
+        let level = +panel.querySelector('input[name="level"]:checked').value;
         document.getElementById("total-" + id).innerHTML = Math.round(((innovation + risk) / level) * 10)
     })
 }
@@ -13,10 +15,12 @@ techdebt_total.forEach((element) => {
     element.addEventListener('change', window.techdebtMeasurement)
 });
 
+// Формула расчёта оценки для багов
 function bugMeasurement() {
-    let mass = +document.querySelector('input[name="mass"][panel_index="0"]:checked').value;
-    let block = +document.querySelector('input[name="block"][panel_index="0"]:checked').value;
-    let critical = +document.querySelector('input[name="critical"][panel_index="0"]:checked').value;
+    let panel = document.getElementById('panel-0');
+    let mass = +panel.querySelector('input[name="mass"]:checked').value;
+    let block = +panel.querySelector('input[name="block"]:checked').value;
+    let critical = +panel.querySelector('input[name="critical"]:checked').value;
     document.getElementById("total-0").innerHTML = mass * block * critical
 }
 
@@ -25,18 +29,56 @@ bug_total.forEach((element) => {
     element.addEventListener('change', window.bugMeasurement)
 });
 
-// Not in use yet
-// function copyToClipboard() {
-//   let area = document.createElement('textarea');
-//
-//   document.body.appendChild(area);
-//     area.value = document.getElementById("total").textContent;
-//     area.select();
-//     document.execCommand("copy");
-//   document.body.removeChild(area);
-// }
-//
-// const copyElement = document.querySelectorAll('[data-action="copyElement"]');
-// copyElement.forEach((element) => {
-//     element.addEventListener('click', window.copyToClipboard)
-// });
+
+// Определяем id выбранного таба
+function getCurrentId() {
+    // Ищем все элементы с классом panel
+    const panels = document.querySelectorAll('.panel');
+
+    // Перебираем найденные элементы
+    for (const panel of panels) {
+        // Проверяем, является ли его display равным 'block'
+        if (getComputedStyle(panel).display === 'block') {
+            return panel.getAttribute('id').split('-')[1];
+        }
+    }
+}
+
+// Собираем текст из выбранных опций
+function getSelectedOptions() {
+    let panel_id = window.getCurrentId();
+
+    let final_text = [];
+
+    final_text.push('Категория — ' + document.getElementById('tab-' + panel_id).textContent)
+
+    let criterias_type = panel_id === '0' ? ["mass", "block", "critical"] : ["innovation", "risk", "level"]
+
+    let panel = document.getElementById('panel-' + panel_id);
+    let criterias = panel.querySelectorAll("[class='panel-subtitle']")
+    criterias.forEach((criteria, key) => {
+        let selected_id = panel.querySelector('input[name="' + criterias_type[key] + '"]:checked').id;
+        let value = panel.querySelector('label[for="' + selected_id +'"]').innerHTML;
+        final_text.push(criteria.textContent + ' ' + value)
+    })
+
+    final_text.push('Итоговая оценка: ' + document.getElementById('total-' + panel_id).textContent)
+    return final_text
+}
+
+// Копируем итоговый текст в буфер обмена
+function copyResultsToClipboard() {
+    let final_text = getSelectedOptions()
+
+    let area = document.createElement('textarea');
+    document.body.appendChild(area);
+    area.value = final_text.join('\n');
+    area.select();
+    document.execCommand("copy");
+    document.body.removeChild(area);
+}
+
+const copyElement = document.querySelectorAll('[data-action="copyElement"]');
+copyElement.forEach((element) => {
+    element.addEventListener('click', window.copyResultsToClipboard)
+});
